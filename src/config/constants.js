f4ad18171a008c596ebc6978f9770f0aee33a2c4
fullstackdev215 <genius.developer.63@gmail.com>
2021-06-09 18:33:38 +0300

    switch network

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,16 +8,18 @@ export const ORDER_DIRS = {
 export const PAGE_SIZE = 5;
 
 // MAINNET API ADDRESS
-// export const API_URL_BEFORE = "https://api.opensea.io/api/v1/assets?offset="
-// export const API_URL_AFTER = "&order_direction=asc&limit=50&collection=nonfungibleapes"
-// export const CHAIN_ID = '0x1'
+export const API_URL_BEFORE = "https://api.opensea.io/api/v1/assets?offset="
+export const API_URL_AFTER = "&order_direction=asc&limit=50&collection=nonfungibleapes"
+export const APETOKEN_ADDRESS = "0x495f947276749ce646f68ac8c248420045cb7b5e"
+export const STAKINGPOOL_ADDRESS = "0x5735f0c13ddeb10d824437ea01f706d756cdd011"
+export const CHAIN_ID = '0x1'
 
 // RINKEBY API ADDRESS MY
-export const API_URL_BEFORE = "https://rinkeby-api.opensea.io/api/v1/assets?offset="
-export const API_URL_AFTER = "&asset_contract_address=0x42f8666e87e509258e272d7c233cde11af78a8b1&order_direction=desc&limit=50"
-export const APETOKEN_ADDRESS = "0x42F8666E87E509258E272D7C233cDE11AF78a8B1"
-export const STAKINGPOOL_ADDRESS = "0x9F8be06E79b370Ff26dFCb4C00D872763B70FB01"
-export const CHAIN_ID = '0x4'
+// export const API_URL_BEFORE = "https://rinkeby-api.opensea.io/api/v1/assets?offset="
+// export const API_URL_AFTER = "&asset_contract_address=0x42f8666e87e509258e272d7c233cde11af78a8b1&order_direction=desc&limit=50"
+// export const APETOKEN_ADDRESS = "0x42F8666E87E509258E272D7C233cDE11AF78a8B1"
+// export const STAKINGPOOL_ADDRESS = "0x9F8be06E79b370Ff26dFCb4C00D872763B70FB01"
+// export const CHAIN_ID = '0x4'
 
 // LOCAL ADDRESS
 // export const APETOKEN_ADDRESS = "0x6FDC2a3F54f219a20e5F49B008fA881Be8B10D92";
@@ -27,5 +29,5 @@ export const CHAIN_ID = '0x4'
 // export const CHAIN_ID = "0x539";
 
 // API SERVER URL
-// export const API_URL = "https://app.nonfungibleapes.wtf/api/";
-export const API_URL = "http://localhost:8000/api/";
+export const API_URL = "https://app.nonfungibleapes.wtf/api/";
+// export const API_URL = "http://localhost:8000/api/";
