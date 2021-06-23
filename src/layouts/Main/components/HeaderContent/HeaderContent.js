6cef475ecb574d59aca07aba8cf3943186e74583
fullstackdev215 <genius.developer.63@gmail.com>
2021-06-23 12:18:46 +0200

    connect event

--- src/layouts/Main/components/HeaderContent/HeaderContent.js
+++ src/layouts/Main/components/HeaderContent/HeaderContent.js
@@ -1,27 +1,33 @@
-import { useContext } from 'react'
-import { Box, Hidden, Toolbar, Button } from '@material-ui/core'
+import { useContext } from "react";
+import { Box, Hidden, Toolbar, Button } from "@material-ui/core";
 
-import Avatar from './Avatar'
-import Logo from './Logo'
-import { ContractContext } from '../../../../context/ContractContext'
+import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { ContractContext } from "../../../../context/ContractContext";
 
 const HeaderContent = (props) => {
+  const { connectWallet, account } = useContext(ContractContext);
 
-	const { connectWallet } = useContext(ContractContext);
+  return (
+    <Toolbar>
+      <Logo />
 
-	return (
-		<Toolbar>
-			<Logo />
+      <Box flexGrow={1} />
 
-			<Box flexGrow={1} />
+      <Button
+        variant="contained"
+        disabled={account}
+        color="primary"
+        onClick={connectWallet}
+      >
+        <p style={{overflow: 'hidden', textOverflow: 'ellipsis'}}>{account === "" ? "Connect" : account}</p>
+      </Button>
 
-			<Button variant="contained" color="primary" onClick={connectWallet}>Connect</Button>
+      <Avatar />
 
-			<Avatar />
+      <Hidden lgUp>{props.sidebarTrigger}</Hidden>
+    </Toolbar>
+  );
+};
 
-			<Hidden lgUp>{props.sidebarTrigger}</Hidden>
-		</Toolbar>
-	)
-}
-
-export default HeaderContent
+export default HeaderContent;
